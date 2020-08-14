04068ed78a27047f69e407fc22e8287c10b72057
Kyle Hansen <1hansen.kyle@gmail.com>
2020-08-13 18:53:17 -0600

    javascript 3 afternoon project

--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,7 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers = mixedNumbers.filter(element => element % 2 === 0)
 
 
 
@@ -44,7 +44,7 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(element => element * 1.07);
 
 
 
@@ -63,9 +63,11 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
-
-
+//  = populations.reduce(/* Provide Your Callback Here */)
+const reducer1 = (acc, element) => {
+  return element + acc
+}
+let totalPopulation = populations.reduce(reducer1, 0)
 
 ////////// PROBLEM 4 //////////
 
@@ -89,7 +91,7 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter(e => e['CP'] > 200)// = monstersInYourPocket.filter(/* Provide Your Callback Here */)
 
 
 
@@ -106,7 +108,7 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+let orderTotals = orders.map(element => element.price + (element['price']*element['tax']) ) // Code here
 
 
 
@@ -126,6 +128,11 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
-
-
+ //Code Here
+const reducer = (accumulator, element) => {
+  if(element.owner === 'Bob'){
+    accumulator += element.price
+  }
+  return accumulator
+}
+let bobsTotal = purchases.reduce(reducer, 0)
